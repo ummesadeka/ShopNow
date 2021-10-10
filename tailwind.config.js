9fc04e3f2f035f00285b467b3392ba273e5f0c36
ummesadeka <sadekaahmed53@gmail.com>
2021-10-10 15:40:43 +0600

    header finished

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens:{
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px'
+    },
+    container:{
+      center: true,
+      padding: '1rem'
+    },
+
+    extend: {
+      fontFamily:{
+        poppins: "'Poppins', sans-serif",
+        roboto: "'Roboto', sans-serif"
+      },
+      colors:{
+        'primary' : '#4BC0C8',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
